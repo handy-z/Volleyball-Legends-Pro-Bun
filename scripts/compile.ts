7ea5bfc441ed8c1d60315d7a8138895fa4220baa
wuw-shz <wuw62481@gmail.com>
2026-01-03 17:52:13 +0700

    @wuw-shz 03/01/26 x62

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -19,13 +19,6 @@ export async function runCompile(): Promise<boolean> {
     sourcemap: "none",
     compile: {
       target: "bun-windows-x64",
-      windows: {
-        title: `VBL Pro v${version}`,
-        description: "Volleyball Legends Pro - Bun",
-        publisher: "Handy",
-        version,
-        hideConsole: true,
-      },
       outfile: `vbl-pro-bun-v${version}`,
     },
   };
